4f445d6390e630ad0392e7c07dec559f4b47d9ec
Aydar N <me@aydar.media>
2023-02-04 22:40:38 +0300

    major advancements

--- mout/src/script.js
+++ mout/src/script.js
@@ -8,6 +8,7 @@ import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader.js'
  */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
+const cursorSub = document.querySelector('div.cursor-sub')
 
 // Scene
 const scene = new THREE.Scene()
@@ -36,15 +37,29 @@ window.addEventListener('resize', () =>
 })
 
 // Cursor
-const cursor = {
-    x: 0,
-    y: 0
+let cursor = {
+    x: {
+        center: 0,
+        corner: 0,
+        cornerCurrent: 0
+    },
+    y: {
+        center: 0,
+        corner: 0,
+        cornerCurrent: 0
+    }
 }
 
 window.addEventListener('mousemove', (event) =>
 {
-    cursor.x = event.clientX / sizes.width - 0.5
-    cursor.y = - (event.clientY / sizes.height - 0.5)
+    cursor.x.corner = event.clientX
+    cursor.y.corner = event.clientY
+    cursor.x.center = event.clientX / sizes.width - 0.5
+    cursor.y.center = - (event.clientY / sizes.height - 0.5)
+    if (cursor.x.cornerCurrent == 0) {
+        cursor.x.cornerCurrent = event.clientX
+        cursor.y.cornerCurrent = event.clientY
+    }
 })
 
 /**
@@ -67,22 +82,21 @@ scene.background = new THREE.Color( 0xdbd7d2 );
 
 const hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
 hemiLight.position.set( 0, 200, 0 );
-scene.add( hemiLight );
+scene.add(hemiLight);
 
 const spotLight = new THREE.SpotLight( 0xffffff, 0.4 );
 spotLight.angle = Math.PI / 5;
 spotLight.penumbra = 0.8;
 spotLight.position.set( 0, 5, 5 );
-spotLight.castShadow = true;
+spotLight.castShadow = false;
 spotLight.shadow.camera.near = 3;
 spotLight.shadow.camera.far = 36;
 spotLight.shadow.mapSize.width = 2048;
 spotLight.shadow.mapSize.height = 2048;
 scene.add(spotLight)
-// scene.add( new THREE.CameraHelper( spotLight.shadow.camera ) );
 
 const loader = new FBXLoader();
-loader.load('/Thinker_v9.fbx', function (object) {
+loader.load('/3d/Thinker_v9.fbx', function (object) {
     object.rotation.y = 1
     object.traverse( function ( child ) {
         if ( child.isMesh ) {
@@ -111,7 +125,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas,
     antialias: true
 })
-renderer.shadowMap.enabled = true
+renderer.shadowMap.enabled = false
 renderer.shadowMap.type = THREE.PCFSoftShadowMap
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -120,22 +134,31 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  * Animate
  */
 const clock = new THREE.Clock()
+let TPF = 0
+let last = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    TPF = Math.round((elapsedTime - last) * 100) / 100
+    last = elapsedTime
 
-    // Update controls
-    // controls.update()
-
-    camera.position.x = cursor.x * 0.8
-    camera.position.y = (cursor.y * 1.8) + 1
+    camera.position.x = cursor.x.center * 0.8
+    camera.position.y = cursor.y.center + 1.5
     camera.lookAt(new THREE.Vector3(0, 2.4, 0))
 
-    // Render
     renderer.render(scene, camera)
 
-    // Call tick again on the next frame
+    cursor.x.cornerCurrent += (cursor.x.corner - cursor.x.cornerCurrent) * (TPF * 12);
+    cursor.y.cornerCurrent += (cursor.y.corner - cursor.y.cornerCurrent) * (TPF * 12);
+    const t = `translate3d(${cursor.x.cornerCurrent}px,${cursor.y.cornerCurrent}px,0px)`;
+    let s = cursorSub.style;
+
+    s['transform'] = t;
+    s['webkitTransform'] = t;
+    s['mozTransform'] = t;
+    s['msTransform'] = t;
+
     window.requestAnimationFrame(tick)
 }
 
